04eff5ed62b23d4538567e50040e9c805e39ea95
kusum sikhwal <kusumsikhwal44@gmail.com>
2021-04-09 08:00:59 +0530

    first commit

---
+++ intro.py
@@ -0,0 +1,13 @@
+first_name = input("First Name: ")
+last_name = input("Last Name: ")
+place = input("City: ")
+Class = input("Standard: ")
+hobby = input("Hobby: ")
+dream = input("Dream :")
+
+
+intro = f"HELLO , i am {first_name} {last_name}\ 
+i belong from {place}\
+i am studing in {Class}\ i love {hobby}\
+i am want to bacome  a {dream}"
+print(intro)
